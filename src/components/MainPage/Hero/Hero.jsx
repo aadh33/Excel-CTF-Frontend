061097c5029ce5a601d24b0fa602ab834978029a
Adithyan Manoj <97414535+aadh33@users.noreply.github.com>
2023-11-15 00:01:17 +0530

    latest-register-page

--- src/components/MainPage/Hero/Hero.jsx
+++ src/components/MainPage/Hero/Hero.jsx
@@ -1,14 +1,20 @@
+// Hero.js
 import "./Hero.css";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
-	return (
-		<div className='main__page_container'>
-			<div className='main__text_container'>
-				<h1 className='evnt__name_quest'>Cyber Quest</h1>
-				<h1 className='evnt__name_quest'>CTF Challenge</h1>
-				<h2>November 21st to November 23rd</h2>
-			</div>
-			<button className='reg__btn'>Register</button>
-		</div>
-	);
+    return (
+        <div className='main__page_container'>
+            <div className='main__text_container'>
+                <h1 className='evnt__name_quest'>Cyber Quest</h1>
+                <h1 className='evnt__name_quest'>CTF Challenge</h1>
+                <h2>November 21st to November 23rd</h2>
+            </div>
+            {/* Wrap the button with Link to navigate to "/register" */}
+            <Link to="/register" className='reg__btn'>
+                Register
+            </Link>
+        </div>
+    );
 }
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,70 @@
+import React, { useState, useEffect } from "react";
 import "./RegisterPage.css";
 
-export default function RegisterPage() {
+const RegisterPage = () => {
+    const [countdown, setCountdown] = useState("");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            const end = new Date(now.getFullYear(), 10, 21); // Month is 0-indexed (10 = November)
+            const distance = end - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Output the result in the countdown timer
+            setCountdown(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+
+            // If the countdown is over, clear the interval
+            if (distance < 0) {
+                clearInterval(interval);
+                setCountdown("Challenge has started!");
+            }
+        }, 1000);
+
+        // Clean up the interval on component unmount
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className='register__container'>
-            REGISTER
+        <div className="register-container">
+            <h4>Coming Soon</h4>
+            <div className="reg-container">
+                <p>Cyber Quest</p>
+                <p className="first-page">Challenge starts on 21st November</p>
+                <p>Register now to be notified before challenge starts</p>
+                <div className="reg-button">Register Now!</div>
+                <div className="countdown">Starts in: {countdown}</div>
+            </div>
         </div>
     );
-}
+};
+
+export default RegisterPage;
+
+
+// import React from "react";
+// import "./RegisterPage.css";
+// const RegisterPage = () => {
+//     return (
+//         <div className="register-container">
+//           <h4> Coming Soon</h4>
+//           <div className="reg-container">
+//           <p>Cyber Quest</p>
+//           <p>Challenge starts on 21st November</p>
+//           <p>Register now to be notified before challenge starts</p>
+//           <div className="reg-button">Register Now!</div>
+//           <div className="countdown">Starts in :
+//            </div>
+//           </div>
+//         </div>
+
+//     );
+ 
+// };
+  
+//   export default RegisterPage;
