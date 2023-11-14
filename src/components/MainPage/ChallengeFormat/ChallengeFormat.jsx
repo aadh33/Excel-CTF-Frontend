f27ce1f2d01a436d35e106074365f02b49583d2e
Diya Therese Shibu <diyathereseshibu@gmail.com>
2023-11-14 21:34:15 +0530

    first commit

--- src/components/MainPage/ChallengeFormat/ChallengeFormat.jsx
+++ src/components/MainPage/ChallengeFormat/ChallengeFormat.jsx
@@ -1,12 +1,4 @@
-/*import "./ChallengeFormat.css";
 
-export default function ChallengeFormat() {
-	return (
-        <div className='challenge__format__container'>
-            CHALLENGE FORMAT
-        </div>
-    );
-}*/
 import React from 'react';
 import './ChallengeFormat.css';
 
